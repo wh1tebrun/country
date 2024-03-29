62e825016caca47c715e4c3673dc22448ef3104e
Ege Tekin <ege33tekin@outlook.com>
2024-03-29 15:08:59 +0100

    version 3.5

--- assets/js/highscores.js
+++
@@ -1,9 +0,0 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-// Show Leaderboard
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
